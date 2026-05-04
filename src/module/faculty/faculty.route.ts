92ee9bc838b535d2ea6fb2eb36d0440f14d75899
coding-cameroon <codingcameroonweb@gmail.com>
2026-05-04 18:49:27 +0100

    faculty endpoint fix

--- src/module/faculty/faculty.route.ts
+++ src/module/faculty/faculty.route.ts
@@ -8,7 +8,6 @@ router.post("/new", facultyController.createFaculty);
 router.get("/:facultyId", facultyController.getFacultyById);
 router.patch("/:facultyId", facultyController.updateFaculty);
 router.delete("/:facultyId", facultyController.deleteFaculty);
-router.get("/:levelId", facultyController.getFacultiesByLevelId);
 router.get("/:levelId/level", facultyController.getFacultiesByLevelId);
 
 export { router as facultyRouter };
